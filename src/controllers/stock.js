cc890ac8d509fe7216df1c95ce88939080ca3396
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-13 13:44:10 +0100

    Lint and style fix

--- src/controllers/stock.js
+++ src/controllers/stock.js
@@ -1,23 +1,22 @@
 import StockService from '../services/stock';
 
 const stockService = new StockService();
-
 export class StockController {
-    async getStocks(req, res, next) {
-        try {
-            const stocks = await stockService.getStocks();
-            res.status(200).send(stocks)
-        } catch (error) {
-            next(error);
-        }
+  async getStocks(req, res, next) {
+    try {
+      const stocks = await stockService.getStocks();
+      res.status(200).send(stocks);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async conditionalFindStock(req, res, next) {
-        try {
-            const stocks = await stockService.stockParamSearch(req.query);
-            res.status(200).send(stocks)
-        } catch (error) {
-            next(error);
-        }
+  async conditionalFindStock(req, res, next) {
+    try {
+      const stocks = await stockService.stockParamSearch(req.query);
+      res.status(200).send(stocks);
+    } catch (error) {
+      next(error);
     }
+  }
 }
--- src/services/order.js
+++ src/services/order.js
@@ -2,80 +2,79 @@ import db from '../models';
 import DbService from './dbservice';
 import CommonService from './common';
 import AuthUtils from '../utils/auth';
-import { orderfields } from '../utils/tableFields'
+import { orderfields } from '../utils/tableFields';
 import { HttpError } from '@src/middlewares/api-error-validator';
 
 const { Order, Order_items } = db;
 const { addEntity, findMultipleByKey } = DbService;
 
 export default class OrderService {
-    async createOrder(data, { name }) {
-        const {
-            items,
-            account,
-            vatAmount,
-            customerId,
-            warehouseId,
-            totalAmount,
-            deliveryDate,
-            subTotalAmount,
-        } = data;
+  async createOrder(data, { name }) {
+    const {
+      items,
+      account,
+      vatAmount,
+      customerId,
+      warehouseId,
+      totalAmount,
+      deliveryDate,
+      subTotalAmount,
+    } = data;
 
-        const orderBodyData = {
-            account,
-            vatAmount,
-            customerId,
-            totalAmount,
-            warehouseId,
-            deliveryDate,
-            subTotalAmount,
-            createdBy: name,
-            status: 'captured',
-            salesOrderId: CommonService.generateReference('CTO_'),
-        };
+    const orderBodyData = {
+      account,
+      vatAmount,
+      customerId,
+      totalAmount,
+      warehouseId,
+      deliveryDate,
+      subTotalAmount,
+      createdBy: name,
+      status: 'captured',
+      salesOrderId: CommonService.generateReference('CTO_'),
+    };
 
-        const order = await addEntity(Order, {...orderBodyData });
+    const order = await addEntity(Order, { ...orderBodyData });
 
-        const orderItems = items.map((stock) => {
-            return Object.assign({}, {
-                orderId: order.id,
-                total: stock.total,
-                cases: stock.cases,
-                productCode: stock.productCode,
-                productName: stock.productName,
-            });
-        });
+    const orderItems = items.map((stock) => {
+      return Object.assign(
+        {},
+        {
+          orderId: order.id,
+          total: stock.total,
+          cases: stock.cases,
+          productCode: stock.productCode,
+          productName: stock.productName,
+        }
+      );
+    });
 
-        const orderitems = await Order_items.bulkCreate(orderItems);
-        order.item = orderitems;
+    const orderitems = await Order_items.bulkCreate(orderItems);
+    order.item = orderitems;
 
-        return order;
-    }
+    return order;
+  }
 
-    async getAllOrders() {
-        const data = await Order.findAll({ include: ['orderItems'] });
-        return data;
-    }
+  async getAllOrders() {
+    const data = await Order.findAll({ include: ['orderItems'] });
+    return data;
+  }
 
+  async queryOrders({ id, warehouseId, status }) {
+    let whereStatement = {};
+    if (id) whereStatement.id = id;
+    if (warehouseId) whereStatement.warehouseId = warehouseId;
+    if (status) whereStatement.status = status;
 
-    async queryOrders({ id, warehouseId, status }) {
-        let whereStatement = {};
-        if (id)
-            whereStatement.id = id;
-        if (warehouseId)
-            whereStatement.warehouseId = warehouseId;
-        if (status)
-            whereStatement.status = status;
+    const data = await Order.findAll({
+      where: whereStatement,
+      include: ['orderItems'],
+    });
+    return data;
+  }
 
-        const data = await Order.findAll({
-            where: whereStatement,
-            include: ['orderItems'],
-        });
-        return data;
-    }
-
-    async printOrders(res) {
-        const data = await findMultipleByKey(Order);
-        await AuthUtils.downloadResource(res, 'orders.csv', orderfields, data);
-    }
+  async printOrders(res) {
+    const data = await findMultipleByKey(Order);
+    await AuthUtils.downloadResource(res, 'orders.csv', orderfields, data);
+  }
 }

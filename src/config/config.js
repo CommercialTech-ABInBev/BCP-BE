1d05f26cf3ccb9fb760ac1e8d22b737239f109ad
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-14 16:40:44 +0100

    bug fix 4

--- src/config/config.js
+++ src/config/config.js
@@ -1,35 +1,35 @@
-import env from './env';
+const env = require('./env');
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = env;
 
 module.exports = {
-  production: {
-    host: DB_HOST,
-    database: DB_NAME,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
-      },
+    production: {
+        host: DB_HOST,
+        database: DB_NAME,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+            },
+        },
     },
-  },
-  development: {
-    host: DB_HOST,
-    database: DB_NAME,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
-      },
+    development: {
+        host: DB_HOST,
+        database: DB_NAME,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+            },
+        },
     },
-  },
-  test: {
-    url: 'env.DATABASE_URL_TEST',
-    dialect: 'mysql',
-  },
+    test: {
+        url: 'env.DATABASE_URL_TEST',
+        dialect: 'mysql',
+    },
 };

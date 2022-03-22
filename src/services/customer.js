fe234909d6e23df43811b944f1144b297493cd42
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-22 13:01:01 +0100

    paginate customer query

--- src/services/customer.js
+++ src/services/customer.js
@@ -9,59 +9,56 @@ const { Order, Order_items, Truck, CustomerAddress, User, Customer } = db;
 const { addEntity, findMultipleByKey, updateByKey, findByKeys } = DbService;
 
 export default class CustomerService {
-  async getOrdersByCustomerId(key) {
-    try {
-      const entities = await Customer.findOne({
-        include: [
-          {
-            model: CustomerAddress,
-            as: 'address',
-          },
-          {
-            model: Order,
-            as: 'orders',
-            include: ['orderItems'],
-          },
-        ],
-        where: key,
-      });
-      return entities;
-    } catch (error) {
-      console.error(error);
-      throw new Error(error);
+    async getOrdersByCustomerId(key) {
+        try {
+            const entities = await Customer.findOne({
+                include: [{
+                        model: CustomerAddress,
+                        as: 'address',
+                    },
+                    {
+                        model: Order,
+                        as: 'orders',
+                        include: ['orderItems'],
+                    },
+                ],
+                where: key,
+            });
+            return entities;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
+        }
     }
-  }
 
-  async searchCustomer(query) {
-    let options = {
-      where: {
-        [sequelize.Op.or]: [
-          {
-            customerId: {
-              [sequelize.Op.like]: '%' + query + '%',
+    async searchCustomer(query) {
+        let options = {
+            where: {
+                [sequelize.Op.or]: [{
+                        customerId: {
+                            [sequelize.Op.like]: '%' + query + '%',
+                        },
+                    },
+                    {
+                        customerName: {
+                            [sequelize.Op.like]: '%' + query + '%',
+                        },
+                    },
+                ],
             },
-          },
-          {
-            customerName: {
-              [sequelize.Op.like]: '%' + query + '%',
-            },
-          },
-        ],
-      },
-      include: [
-        {
-          model: CustomerAddress,
-          as: 'address',
-        },
-        {
-          model: Order,
-          as: 'orders',
-          include: ['orderItems'],
-        },
-      ],
-    };
+            include: [{
+                    model: CustomerAddress,
+                    as: 'address',
+                },
+                {
+                    model: Order,
+                    as: 'orders',
+                    include: ['orderItems'],
+                },
+            ],
+        };
 
-    const customers = await Customer.findAll(options);
-    return customers;
-  }
+        const { count, rows } = await Customer.findAndCountAll(options);
+        return { TotalCount: count, customer: rows };
+    }
 }

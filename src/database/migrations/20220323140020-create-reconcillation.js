c64511cea1ed07abfad779fb891a6e57c6a5054a
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-23 16:36:36 +0100

    convert reconcillation to es6 mod

--- src/database/migrations/20220323140020-create-reconcillation.js
+++ src/database/migrations/20220323140020-create-reconcillation.js
@@ -1,39 +1,37 @@
 'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
+export function up(queryInterface, Sequelize) {
     return queryInterface.createTable('Reconcillations', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      account: {
-        type: Sequelize.STRING,
-      },
-      stock: {
-        type: Sequelize.STRING,
-      },
-      warehouse: {
-        type: Sequelize.STRING,
-      },
-      amount: {
-        type: Sequelize.INTEGER,
-      },
-      quantity: {
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        account: {
+            type: Sequelize.STRING,
+        },
+        stock: {
+            type: Sequelize.STRING,
+        },
+        warehouse: {
+            type: Sequelize.STRING,
+        },
+        amount: {
+            type: Sequelize.INTEGER,
+        },
+        quantity: {
+            type: Sequelize.INTEGER,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
     });
-  },
-  down: (queryInterface, Sequelize) => {
+}
+export function down(queryInterface, Sequelize) {
     return queryInterface.dropTable('Reconcillations');
-  },
-};
+}

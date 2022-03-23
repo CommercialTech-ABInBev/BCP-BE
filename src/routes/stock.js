f7af1d7e7e278c8fb63c323671b8fc9ebe743af3
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-23 12:23:51 +0100

    implement csv download order endpoint

--- src/routes/stock.js
+++ src/routes/stock.js
@@ -10,39 +10,43 @@ const router = Router();
 const stockcontroller = new StockController();
 
 router.get(
-  '/fetchStocks',
-  authMiddleware,
-  verifyRoles(['cic']),
-  stockcontroller.getStocks
+    '/fetchStocks',
+    authMiddleware,
+    verifyRoles(['cic']),
+    stockcontroller.getStocks
 );
 router.get(
-  '/searchStock',
-  authMiddleware,
-  verifyRoles(['cic']),
-  stockcontroller.conditionalFindStock
+    '/searchStock',
+    authMiddleware,
+    verifyRoles(['cic']),
+    stockcontroller.conditionalFindStock
 );
 
 router.get(
-  '/getStockPrice',
-  authMiddleware,
-  verifyRoles(['cic']),
-  stockcontroller.getStockPrices
+    '/getStockPrice',
+    authMiddleware,
+    verifyRoles(['cic']),
+    stockcontroller.getStockPrices
 );
 
 router.get(
-  '/queryStock',
-  authMiddleware,
-  verifyRoles(['cic', 'whm', 'admin']),
-  stockcontroller.searchStocks
+    '/queryStock',
+    authMiddleware,
+    verifyRoles(['cic', 'whm', 'admin']),
+    stockcontroller.searchStocks
 );
 
 router.get(
-  '/getWHMstocks',
-  authMiddleware,
-  verifyRoles(['whm']),
-  stockcontroller.getWHMstocks
+    '/getWHMstocks',
+    authMiddleware,
+    verifyRoles(['whm']),
+    stockcontroller.getWHMstocks
 );
 
-router.put('/addStock', stockcontroller.addStock);
+router.put('/addStock', authMiddleware,
+    verifyRoles(['whm']), stockcontroller.addStock);
+
+router.get('/downloadStocks', authMiddleware,
+    verifyRoles(['cic', 'admin']), stockcontroller.csvDownloadStock)
 
 export default router;
--- src/utils/tableFields.js
+++ src/utils/tableFields.js
@@ -1,30 +1,49 @@
-export const orderfields = [
-  {
-    label: 'Sales order no',
-    value: 'salesOrderId',
-  },
-  {
-    label: 'Warehouse code',
-    value: 'warehouseId',
-  },
-  {
-    label: 'Account',
-    value: 'account',
-  },
-  {
-    label: 'Created By',
-    value: 'createdBy',
-  },
-  {
-    label: 'Total Amount',
-    value: 'totalAmount',
-  },
-  {
-    label: 'Status',
-    value: 'status',
-  },
-  {
-    label: 'Date Created',
-    value: 'createdAt',
-  },
+export const orderfields = [{
+        label: 'Sales order no',
+        value: 'salesOrderId',
+    },
+    {
+        label: 'Warehouse code',
+        value: 'warehouseId',
+    },
+    {
+        label: 'Account',
+        value: 'account',
+    },
+    {
+        label: 'Created By',
+        value: 'createdBy',
+    },
+    {
+        label: 'Total Amount',
+        value: 'totalAmount',
+    },
+    {
+        label: 'Status',
+        value: 'status',
+    },
+    {
+        label: 'Date Created',
+        value: 'createdAt',
+    },
 ];
+
+export const stockField = [{
+    label: 'Volume',
+    value: 'size',
+}, {
+    label: 'Stock Code',
+    value: 'stockCode',
+}, {
+    label: 'Stock Name',
+    value: 'description',
+}, {
+    label: 'Container',
+    value: 'packageType',
+}, {
+    label: 'In Transit',
+    value: 'inTransitCs',
+}, {
+    label: 'Free Stock',
+    value: 'freeStockCs',
+}]

310901e75ce41d2d68c4c99c70bac12aa55d2ba8
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-03-24 18:38:03 +0100

    add pallet field to order item

--- src/validations/order.validation.js
+++ src/validations/order.validation.js
@@ -1,21 +1,22 @@
 import Joi from 'joi';
 
 export const createOrderchema = {
-  warehouseId: Joi.string().required(),
-  customerId: Joi.string().required(),
-  account: Joi.string().required(),
-  deliveryDate: Joi.string().required(),
-  vatAmount: Joi.number().required(),
-  subTotalAmount: Joi.number().required(),
-  totalAmount: Joi.number().required(),
-  items: Joi.array()
-    .items(
-      Joi.object({
-        cases: Joi.number().required(),
-        total: Joi.number().required(),
-        productCode: Joi.string().required(),
-        productName: Joi.string().required(),
-      })
-    )
-    .required(),
+    warehouseId: Joi.string().required(),
+    customerId: Joi.string().required(),
+    account: Joi.string().required(),
+    deliveryDate: Joi.string().required(),
+    vatAmount: Joi.number().required(),
+    subTotalAmount: Joi.number().required(),
+    totalAmount: Joi.number().required(),
+    items: Joi.array()
+        .items(
+            Joi.object({
+                pallets: Joi.number(),
+                cases: Joi.number().required(),
+                total: Joi.number().required(),
+                productCode: Joi.string().required(),
+                productName: Joi.string().required(),
+            })
+        )
+        .required(),
 };

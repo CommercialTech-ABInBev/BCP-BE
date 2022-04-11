c83f011d5eeb0f7837cc8d214f8cb89a39de74fd
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-11 15:12:45 +0100

    implement user permision roles

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,31 +3,33 @@ import { UserController } from '../controllers/user';
 import { validationMiddleware } from '../middlewares/validation';
 import { authMiddleware } from '../middlewares/auth';
 import {
-  signupSchema,
-  loginSchema,
-  resetPassword,
+    signupSchema,
+    loginSchema,
+    resetPassword,
 } from '../validations/auth.validation';
-import { use } from 'chai';
+import { verifyRoles } from '../middlewares/rolemgt';
+
+
 
 const router = Router();
 const usercontroller = new UserController();
 
 router.put('/accept-invite', usercontroller.acceptInvite);
-router.delete('/delete-user', usercontroller.adminDeleteUserProfile);
+router.delete('/delete-user', authMiddleware, verifyRoles(['AM']), usercontroller.adminDeleteUserProfile);
 router.post('/login', validationMiddleware(loginSchema), usercontroller.login);
 router.post(
-  '/send-invite',
-  validationMiddleware(signupSchema),
-  usercontroller.adminSendInvite
+    '/send-invite',
+    validationMiddleware(signupSchema),
+    usercontroller.adminSendInvite
 );
 router.post(
-  '/reset-password',
-  authMiddleware,
-  validationMiddleware(resetPassword),
-  usercontroller.resetPassword
+    '/reset-password',
+    authMiddleware,
+    validationMiddleware(resetPassword),
+    usercontroller.resetPassword
 );
 
-router.patch('/editUser', usercontroller.updateUser);
+router.patch('/editUser', authMiddleware, verifyRoles(['AM']), usercontroller.updateUser);
 
-router.get('/getAllUsers', authMiddleware, usercontroller.getAllUsers);
+router.get('/getAllUsers', authMiddleware, verifyRoles(['AM']), usercontroller.getAllUsers);
 export default router;
--- src/routes/stock.js
+++ src/routes/stock.js
@@ -10,103 +10,103 @@ const router = Router();
 const stockcontroller = new StockController();
 
 router.get(
-  '/adminDashboard',
-  authMiddleware,
-  verifyRoles(['AM', 'WM']),
-  stockcontroller.dashBoard
+    '/adminDashboard',
+    authMiddleware,
+    verifyRoles(['AM']),
+    stockcontroller.dashBoard
 );
 
 router.post(
-  '/check-in',
-  authMiddleware,
-  upload.single('file'),
-  validationMiddleware(checkInSchema),
-  verifyRoles(['AM', 'WM', 'am']),
-  stockcontroller.stockCheckIn
+    '/check-in',
+    authMiddleware,
+    upload.single('file'),
+    validationMiddleware(checkInSchema),
+    verifyRoles(['AM', 'WM']),
+    stockcontroller.stockCheckIn
 );
 
 router.patch(
-  '/approve-check-in',
-  authMiddleware,
-  verifyRoles(['AM', 'WM']),
-  stockcontroller.approveCheckIn
+    '/approve-check-in',
+    authMiddleware,
+    verifyRoles(['AM', 'WM']),
+    stockcontroller.approveCheckIn
 );
 
 router.get(
-  '/get-approved-stocks',
-  authMiddleware,
-  verifyRoles(['AM', 'BM']),
-  stockcontroller.getApprovedStocks
+    '/get-approved-stocks',
+    authMiddleware,
+    verifyRoles(['AM', 'BM']),
+    stockcontroller.getApprovedStocks
 );
 
 router.get(
-  '/get-check-ins',
-  authMiddleware,
-  verifyRoles(['AM', 'BM', 'WM', 'am']),
-  stockcontroller.getAllCheckIns
+    '/get-check-ins',
+    authMiddleware,
+    verifyRoles(['AM', 'BM', 'WM']),
+    stockcontroller.getAllCheckIns
 );
 
 router.post(
-  '/check-out',
-  authMiddleware,
-  verifyRoles(['BM', 'WM', 'AM']),
-  stockcontroller.checkOut
+    '/check-out',
+    authMiddleware,
+    verifyRoles(['BM', 'WM', 'AM']),
+    stockcontroller.checkOut
 );
 
 router.get(
-  '/get-check-outs',
-  authMiddleware,
-  verifyRoles(['AM', 'BM', 'WM']),
-  stockcontroller.getCheckOut
+    '/get-check-outs',
+    authMiddleware,
+    verifyRoles(['AM', 'BM', 'WM']),
+    stockcontroller.getCheckOut
 );
 
 router.patch(
-  '/reject-check-out',
-  authMiddleware,
-  verifyRoles(['AM', 'WM']),
-  stockcontroller.rejectCheckOut
+    '/reject-check-out',
+    authMiddleware,
+    verifyRoles(['AM', 'WM']),
+    stockcontroller.rejectCheckOut
 );
 
 router.patch(
-  '/reject-check-in',
-  authMiddleware,
-  verifyRoles(['AM', 'WM']),
-  stockcontroller.rejectCheckIn
+    '/reject-check-in',
+    authMiddleware,
+    verifyRoles(['AM', 'WM']),
+    stockcontroller.rejectCheckIn
 );
 
 router.get(
-  '/csvDownloadStocksOrCheckIns',
-  authMiddleware,
-  verifyRoles(['AM', 'BM', 'WM']),
-  stockcontroller.csvDownloadStocks
+    '/csvDownloadStocksOrCheckIns',
+    authMiddleware,
+    verifyRoles(['AM', 'BM', 'WM']),
+    stockcontroller.csvDownloadStocks
 );
 
 router.get(
-  '/csvDownloadCheckOuts',
-  authMiddleware,
-  verifyRoles(['AM', 'BM', 'WM']),
-  stockcontroller.csvDownloadCheckOuts
+    '/csvDownloadCheckOuts',
+    authMiddleware,
+    verifyRoles(['AM', 'BM', 'WM']),
+    stockcontroller.csvDownloadCheckOuts
 );
 
 router.patch(
-  '/stock-adjustment',
-  authMiddleware,
-  verifyRoles(['AM']),
-  stockcontroller.stockAdjustment
+    '/stock-adjustment',
+    authMiddleware,
+    verifyRoles(['AM']),
+    stockcontroller.stockAdjustment
 );
 
 router.patch(
-  '/approve-reject-adjustment',
-  authMiddleware,
-  verifyRoles(['AM']),
-  stockcontroller.approveStockAdjustment
+    '/approve-reject-adjustment',
+    authMiddleware,
+    verifyRoles(['AM']),
+    stockcontroller.approveStockAdjustment
 );
 
 router.patch(
-  '/approve-check-out',
-  authMiddleware,
-  verifyRoles(['AM', 'WM']),
-  stockcontroller.approveCheckOut
+    '/approve-check-out',
+    authMiddleware,
+    verifyRoles(['AM', 'WM']),
+    stockcontroller.approveCheckOut
 );
 
 export default router;

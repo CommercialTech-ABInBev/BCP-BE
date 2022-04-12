ca605d7ad5cc828f3c825c8cb63e964a3e62304e
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-12 14:22:24 +0100

    fix up admin dashboard endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,33 +3,46 @@ import { UserController } from '../controllers/user';
 import { validationMiddleware } from '../middlewares/validation';
 import { authMiddleware } from '../middlewares/auth';
 import {
-    signupSchema,
-    loginSchema,
-    resetPassword,
+  signupSchema,
+  loginSchema,
+  resetPassword,
 } from '../validations/auth.validation';
 import { verifyRoles } from '../middlewares/rolemgt';
-
-
 
 const router = Router();
 const usercontroller = new UserController();
 
 router.put('/accept-invite', usercontroller.acceptInvite);
-router.delete('/delete-user', authMiddleware, verifyRoles(['AM']), usercontroller.adminDeleteUserProfile);
+router.delete(
+  '/delete-user',
+  authMiddleware,
+  verifyRoles(['AM']),
+  usercontroller.adminDeleteUserProfile
+);
 router.post('/login', validationMiddleware(loginSchema), usercontroller.login);
 router.post(
-    '/send-invite',
-    validationMiddleware(signupSchema),
-    usercontroller.adminSendInvite
+  '/send-invite',
+  validationMiddleware(signupSchema),
+  usercontroller.adminSendInvite
 );
 router.post(
-    '/reset-password',
-    authMiddleware,
-    validationMiddleware(resetPassword),
-    usercontroller.resetPassword
+  '/reset-password',
+  authMiddleware,
+  validationMiddleware(resetPassword),
+  usercontroller.resetPassword
 );
 
-router.patch('/editUser', authMiddleware, verifyRoles(['AM']), usercontroller.updateUser);
+router.patch(
+  '/editUser',
+  authMiddleware,
+  verifyRoles(['AM']),
+  usercontroller.updateUser
+);
 
-router.get('/getAllUsers', authMiddleware, verifyRoles(['AM']), usercontroller.getAllUsers);
+router.get(
+  '/getAllUsers',
+  authMiddleware,
+  verifyRoles(['AM']),
+  usercontroller.getAllUsers
+);
 export default router;

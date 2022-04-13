2705dfb5bdd29a1696446163f4b80e3b1e0d5e26
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-13 11:06:56 +0100

    add comment field to csv

--- src/utils/tableFields.js
+++ src/utils/tableFields.js
@@ -1,110 +1,110 @@
-export const orderfields = [
-  {
-    label: 'Sales order no',
-    value: 'salesOrderId',
-  },
-  {
-    label: 'Warehouse code',
-    value: 'warehouseId',
-  },
-  {
-    label: 'Account',
-    value: 'account',
-  },
-  {
-    label: 'Customer ID',
-    value: 'customerId',
-  },
-  {
-    label: 'Created By',
-    value: 'createdBy',
-  },
-  {
-    label: 'Total Amount',
-    value: 'totalAmount',
-  },
-  {
-    label: 'Vat Amount',
-    value: 'vatAmount',
-  },
-  {
-    label: 'Truck ID',
-    value: 'truckId',
-  },
-  {
-    label: 'Load ID',
-    value: 'loadId',
-  },
-  {
-    label: 'Product Name',
-    value: 'productName',
-  },
-  {
-    label: 'Cases',
-    value: 'cases',
-  },
-  ,
-  {
-    label: 'Total',
-    value: 'total',
-  },
-  ,
-  {
-    label: 'Volume',
-    value: 'volume',
-  },
-  {
-    label: 'Status',
-    value: 'status',
-  },
+export const orderfields = [{
+        label: 'Sales order no',
+        value: 'salesOrderId',
+    },
+    {
+        label: 'Warehouse code',
+        value: 'warehouseId',
+    },
+    {
+        label: 'Account',
+        value: 'account',
+    },
+    {
+        label: 'Customer ID',
+        value: 'customerId',
+    },
+    {
+        label: 'Created By',
+        value: 'createdBy',
+    },
+    {
+        label: 'Total Amount',
+        value: 'totalAmount',
+    },
+    {
+        label: 'Vat Amount',
+        value: 'vatAmount',
+    },
+    {
+        label: 'Truck ID',
+        value: 'truckId',
+    },
+    {
+        label: 'Load ID',
+        value: 'loadId',
+    },
+    {
+        label: 'Product Name',
+        value: 'productName',
+    },
+    {
+        label: 'Cases',
+        value: 'cases',
+    }, ,
+    {
+        label: 'Total',
+        value: 'total',
+    }, ,
+    {
+        label: 'Volume',
+        value: 'volume',
+    },
+
+    {
+        label: 'Status',
+        value: 'status',
+    },
+    {
+        label: 'Comment',
+        value: 'comment',
+    },
 ];
 
-export const stockField = [
-  {
-    label: 'Volume',
-    value: 'size',
-  },
-  {
-    label: 'Stock Code',
-    value: 'stockCode',
-  },
-  {
-    label: 'Stock Name',
-    value: 'description',
-  },
-  {
-    label: 'Container',
-    value: 'packageType',
-  },
-  {
-    label: 'In Transit',
-    value: 'inTransitCs',
-  },
-  {
-    label: 'Free Stock',
-    value: 'freeStockCs',
-  },
+export const stockField = [{
+        label: 'Volume',
+        value: 'size',
+    },
+    {
+        label: 'Stock Code',
+        value: 'stockCode',
+    },
+    {
+        label: 'Stock Name',
+        value: 'description',
+    },
+    {
+        label: 'Container',
+        value: 'packageType',
+    },
+    {
+        label: 'In Transit',
+        value: 'inTransitCs',
+    },
+    {
+        label: 'Free Stock',
+        value: 'freeStockCs',
+    },
 ];
 
-export const reconcillationField = [
-  {
-    label: 'Account',
-    value: 'account',
-  },
-  {
-    label: 'Stock Code',
-    value: 'stock',
-  },
-  {
-    label: 'Warehouse',
-    value: 'warehouse',
-  },
-  {
-    label: 'Amount',
-    value: 'amount',
-  },
-  {
-    label: 'Cases',
-    value: 'quantity',
-  },
+export const reconcillationField = [{
+        label: 'Account',
+        value: 'account',
+    },
+    {
+        label: 'Stock Code',
+        value: 'stock',
+    },
+    {
+        label: 'Warehouse',
+        value: 'warehouse',
+    },
+    {
+        label: 'Amount',
+        value: 'amount',
+    },
+    {
+        label: 'Cases',
+        value: 'quantity',
+    },
 ];

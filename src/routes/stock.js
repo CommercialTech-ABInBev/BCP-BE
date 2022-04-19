da9e7d5b08d5ff64effac1c5c34dcc2c14775fa8
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-20 00:29:01 +0100

    fix file errors

--- src/routes/stock.js
+++ src/routes/stock.js
@@ -6,67 +6,67 @@ import { StockController } from '../controllers/stock';
 import { validationMiddleware } from '../middlewares/validation';
 
 import {
-  searchStock,
-  addStockSchema,
-  stockPriceSchema,
-  paginationStockSchema,
-  stockSearchParamSchema,
+    searchStock,
+    addStockSchema,
+    stockPriceSchema,
+    paginationStockSchema,
+    stockSearchParamSchema,
 } from '../validations/stock.validation';
 
 const router = Router();
 const stockcontroller = new StockController();
 
 router.get(
-  '/fetchStocks',
-  authMiddleware,
-  verifyRoles(['cic']),
-  stockcontroller.getStocks
+    '/fetchStocks',
+    authMiddleware,
+    verifyRoles(['cic']),
+    stockcontroller.getStocks
 );
 
 router.get(
-  '/searchStock',
-  authMiddleware,
-  verifyRoles(['cic', 'dist']),
-  validationMiddleware(stockSearchParamSchema),
-  stockcontroller.conditionalFindStock
+    '/searchStock',
+    authMiddleware,
+    verifyRoles(['cic', 'dist']),
+    validationMiddleware(stockSearchParamSchema),
+    stockcontroller.conditionalFindStock
 );
 
 router.get(
-  '/getStockPrice',
-  authMiddleware,
-  verifyRoles(['cic', 'dist', 'whm']),
-  validationMiddleware(stockPriceSchema),
-  stockcontroller.getStockPrices
+    '/getStockPrice',
+    authMiddleware,
+    verifyRoles(['cic', 'dist', 'whm']),
+    validationMiddleware(stockPriceSchema),
+    stockcontroller.getStockPrices
 );
 
 router.get(
-  '/queryStock',
-  authMiddleware,
-  verifyRoles(['cic', 'whm', 'dist']),
-  validationMiddleware(searchStock),
-  stockcontroller.searchStocks
+    '/queryStock',
+    authMiddleware,
+    verifyRoles(['cic', 'whm', 'dist']),
+    validationMiddleware(searchStock),
+    stockcontroller.searchStocks
 );
 
 router.get(
-  '/getDepotstocks',
-  authMiddleware,
-  verifyRoles(['whm', 'dist']),
-  validationMiddleware(paginationStockSchema),
-  stockcontroller.getWHMstocks
+    '/getDepotstocks',
+    authMiddleware,
+    verifyRoles(['whm', 'dist']),
+    validationMiddleware(paginationStockSchema),
+    stockcontroller.getWHMstocks
 );
 
 router.put(
-  '/addStock',
-  authMiddleware,
-  verifyRoles(['whm', 'dist']),
-  stockcontroller.addStock
+    '/addStock',
+    authMiddleware,
+    verifyRoles(['whm', 'dist']),
+    stockcontroller.addStock
 );
 
 router.get(
-  '/downloadStocks',
-  authMiddleware,
-  verifyRoles(['cic', 'dist']),
-  stockcontroller.csvDownloadStock
+    '/downloadStocks',
+    authMiddleware,
+    verifyRoles(['cic', 'dist']),
+    stockcontroller.csvDownloadStock
 );
 
 export default router;

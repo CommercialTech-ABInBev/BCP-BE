11c92870b6e092730703d8f19d8c235afbf9412f
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-19 17:42:49 +0100

    Merge branch 'main' of github.com:CommercialTech-ABInBev/BCP-BE into main

--- src/validations/order.validation.js
+++ src/validations/order.validation.js
@@ -1,56 +1,57 @@
 import Joi from 'joi';
 
 export const createOrderchema = {
-  comment: Joi.string(),
-  account: Joi.string().required(),
-  vatAmount: Joi.number().required(),
-  customerId: Joi.string().required(),
-  warehouseId: Joi.string().required(),
-  totalAmount: Joi.number().required(),
-  deliveryDate: Joi.string().required(),
-  subTotalAmount: Joi.number().required(),
-  items: Joi.array()
-    .items(
-      Joi.object({
-        pallets: Joi.number(),
-        cases: Joi.number().required(),
-        total: Joi.number().required(),
-        volume: Joi.number().required(),
-        productCode: Joi.string().required(),
-        productName: Joi.string().required(),
-      })
-    )
-    .required(),
+
+    comment: Joi.string(),
+    account: Joi.string().required(),
+    vatAmount: Joi.number().required(),
+    customerId: Joi.string().required(),
+    warehouseId: Joi.string().required(),
+    totalAmount: Joi.number().required(),
+    deliveryDate: Joi.string().required(),
+    subTotalAmount: Joi.number().required(),
+    items: Joi.array()
+        .items(
+            Joi.object({
+                pallets: Joi.number(),
+                cases: Joi.number().required(),
+                total: Joi.number().required(),
+                volume: Joi.number().required(),
+                productCode: Joi.string().required(),
+                productName: Joi.string().required(),
+            })
+        )
+        .required(),
 };
 
 export const paginationSchema = {
-  page: Joi.number().required(),
-  pageSize: Joi.number(),
+    page: Joi.number().required(),
+    pageSize: Joi.number(),
 };
 
 export const paginateQueryOrder = {
-  id: Joi.string(),
-  status: Joi.string(),
-  loadId: Joi.string(),
+    id: Joi.string(),
+    status: Joi.string(),
+    loadId: Joi.string(),
 };
 
 export const queryId = {
-  id: Joi.string().required(),
+    id: Joi.string().required(),
 };
 
 export const planLoadSchema = {
-  orderId: Joi.array().items(Joi.string()).required(),
-  truckId: Joi.string().required(),
+    orderId: Joi.array().items(Joi.string()).required(),
+    truckId: Joi.string().required(),
 };
 
 export const searchOrder = {
-  search: Joi.string().required(),
-  orderStatus: Joi.string(),
+    search: Joi.string().required(),
+    orderStatus: Joi.string(),
 };
 
 export const rePlanOrderSchema = {
-  loadId: Joi.string().required(),
-  truckId: Joi.string().required(),
+    loadId: Joi.string().required(),
+    truckId: Joi.string().required(),
 };
 
 export const addStockSchema = Joi.array().items();

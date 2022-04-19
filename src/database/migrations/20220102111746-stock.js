e3a4063898f0fae47f0748019d40d8f7b096086a
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-19 20:54:54 +0100

    Modify stocks and checkout schema

--- src/database/migrations/20220102111746-stock.js
+++ src/database/migrations/20220102111746-stock.js
@@ -1,35 +1,51 @@
 'use strict';
 
 export function up(queryInterface, Sequelize) {
-  return queryInterface.createTable('Stocks', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-    },
-    title: Sequelize.STRING,
-    location: Sequelize.STRING,
-    status: Sequelize.STRING,
-    prevQty: Sequelize.INTEGER,
-    currQty: Sequelize.INTEGER,
-    deletedAt: Sequelize.DATE,
-    brand: Sequelize.STRING,
-    stockType: Sequelize.STRING,
-    supportDocFile: Sequelize.STRING,
-    stockAdjustTo: Sequelize.INTEGER,
-    stockAdjustFrom: Sequelize.INTEGER,
-    stockAdjustStatus: Sequelize.STRING,
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-  });
+    return queryInterface.createTable('Stocks', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        title: Sequelize.STRING,
+        location: Sequelize.STRING,
+        status: Sequelize.STRING,
+        prevQty: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        currQty: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        deletedAt: Sequelize.DATE,
+        brand: Sequelize.STRING,
+        stockType: Sequelize.STRING,
+        supportDocFile: Sequelize.STRING,
+        stockAdjustTo: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        stockAdjustFrom: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        stockAdjustStatus: Sequelize.STRING,
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+    });
 }
 export function down(queryInterface, Sequelize) {
-  return queryInterface.dropTable('Stocks');
+    return queryInterface.dropTable('Stocks');
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,9 +3,9 @@ import { UserController } from '../controllers/user';
 import { validationMiddleware } from '../middlewares/validation';
 import { authMiddleware } from '../middlewares/auth';
 import {
-  signupSchema,
-  loginSchema,
-  resetPassword,
+    signupSchema,
+    loginSchema,
+    resetPassword,
 } from '../validations/auth.validation';
 import { verifyRoles } from '../middlewares/rolemgt';
 
@@ -14,35 +14,35 @@ const usercontroller = new UserController();
 
 router.put('/accept-invite', usercontroller.acceptInvite);
 router.delete(
-  '/delete-user',
-  authMiddleware,
-  verifyRoles(['AM']),
-  usercontroller.adminDeleteUserProfile
+    '/delete-user',
+    authMiddleware,
+    verifyRoles(['AM']),
+    usercontroller.adminDeleteUserProfile
 );
 router.post('/login', validationMiddleware(loginSchema), usercontroller.login);
 router.post(
-  '/send-invite',
-  validationMiddleware(signupSchema),
-  usercontroller.adminSendInvite
+    '/send-invite',
+    validationMiddleware(signupSchema),
+    usercontroller.adminSendInvite
 );
 router.post(
-  '/reset-password',
-  authMiddleware,
-  validationMiddleware(resetPassword),
-  usercontroller.resetPassword
+    '/reset-password',
+    authMiddleware,
+    validationMiddleware(resetPassword),
+    usercontroller.resetPassword
 );
 
 router.patch(
-  '/editUser',
-  authMiddleware,
-  verifyRoles(['AM']),
-  usercontroller.updateUser
+    '/editUser',
+    authMiddleware,
+    verifyRoles(['AM']),
+    usercontroller.updateUser
 );
 
 router.get(
-  '/getAllUsers',
-  authMiddleware,
-  verifyRoles(['AM']),
-  usercontroller.getAllUsers
+    '/getAllUsers',
+    authMiddleware,
+    verifyRoles(['AM']),
+    usercontroller.getAllUsers
 );
 export default router;

dff61678d2700ed08ab15aa5980b88e25865ee22
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-20 00:11:24 +0100

    Weed off redundant model

--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,29 @@
 'use strict';
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'Users',
-    {
-      fullName: DataTypes.STRING,
-      role: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      location: DataTypes.STRING,
-      inviteStatus: DataTypes.STRING,
-      emailVerified: DataTypes.BOOLEAN,
-      brand: DataTypes.STRING,
-    },
-    {
-      timestamps: true,
-      paranoid: true,
-    }
-  );
-  User.associate = function (models) {
-    // associations can be defined here
-  };
+    const User = sequelize.define(
+        'Users', {
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+            role: DataTypes.STRING,
+            password: DataTypes.STRING,
+            location: DataTypes.STRING,
+            inviteStatus: DataTypes.STRING,
+            emailVerified: DataTypes.BOOLEAN,
+        }, {
+            timestamps: true,
+            paranoid: true,
+        }
+    );
+    User.associate = function(models) {
+        // associations can be defined here
+    };
 
-  User.prototype.toJSON = function () {
-    var values = Object.assign({}, this.get());
+    User.prototype.toJSON = function() {
+        let values = Object.assign({}, this.get());
 
-    delete values.password;
-    return values;
-  };
+        delete values.password;
+        return values;
+    };
 
-  return User;
+    return User;
 };

b2fa1d14170974f38ec039bb6975c28f62e80ad3
Uche Uzochukwu Mark <50272213+markeu@users.noreply.github.com>
2022-04-21 12:13:52 +0100

    Inncorporate some auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,33 +1,22 @@
 import { Router } from 'express';
 
-import { UserController } from '../controllers/auth';
 import { authMiddleware } from '../middlewares/auth';
+import { UserController } from '../controllers/auth';
 import { validationMiddleware } from '../middlewares/validation';
-import {
-    signupSchema,
-    loginSchema,
-    resetPassword,
-} from '../validations/auth.validation';
-import { verifyRoles } from '../middlewares/rolemgt';
+import { loginSchema, resetPassword, signupSchema } from '../validations/auth.validation';
 
 const router = Router();
 const usercontroller = new UserController();
 
-
-router.delete(
-    '/delete-user',
-    authMiddleware,
-    verifyRoles(['AM']),
-    usercontroller.adminDeleteUserProfile
-);
+router.delete('/delete-user', usercontroller.adminDeleteUserProfile);
 router.post('/login', validationMiddleware(loginSchema), usercontroller.login);
-
 router.post(
     '/reset-password',
     authMiddleware,
     validationMiddleware(resetPassword),
     usercontroller.resetPassword
 );
-
+router.get('/users', usercontroller.getUsers);
+router.post('/user', validationMiddleware(signupSchema), usercontroller.createuser);
 
 export default router;
